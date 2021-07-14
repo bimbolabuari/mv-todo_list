7b511571550e4ec34d652d784a7cac740807354e
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-07-14 23:23:29 +0100

    feat: Add src folder for project source file

---
+++ src/footer.js
@@ -0,0 +1,7 @@
+const footer = document.createElement('footer');
+footer.classList.add('footer');
+footer.innerHTML = `
+<button type="button" class="button">Clear all completed<button>
+`;
+
+export default footer;
---
+++ src/input.js
@@ -0,0 +1,7 @@
+const form = document.createElement('form');
+form.classList.add('form');
+form.innerHTML = `
+    <input type="text" class="input" placeholder="Add to your list..."></input>
+    `;
+
+export default form;
