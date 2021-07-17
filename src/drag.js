74260e5403714cfa1373ed266a5f1e5c003ce4f7
Abimbola Aminat Buari <abimbola.buari@gmail.com>
2021-07-17 04:07:33 +0100

    feat: Add drag and drop functionality

---
+++ src/drag.js
@@ -0,0 +1,37 @@
+import reorderTasks from './sort.js';
+import { getTasksArray, setTasksArray, displayTaskArray } from './tasks.js';
+
+const reRenderTask = (tasks, taskContainerEl) => {
+  while (taskContainerEl.firstChild) {
+    taskContainerEl.removeChild(taskContainerEl.firstChild);
+  }
+  displayTaskArray(tasks, taskContainerEl);
+};
+
+const dragAndDropHandler = () => {
+  let taskDragged;
+  document.addEventListener('dragstart', (event) => {
+    if (event.target.dataset.action === 'drag') {
+      taskDragged = event.target;
+      taskDragged.classList.add('dragstart');
+    }
+  }, false);
+
+  document.addEventListener('dragover', (event) => {
+    event.preventDefault();
+  }, false);
+
+  document.addEventListener('drop', (event) => {
+    event.preventDefault();
+    taskDragged.classList.remove('dragstart');
+    if (event.target.dataset.action === 'drag') {
+      let tasksArray = getTasksArray();
+      tasksArray = reorderTasks(tasksArray, taskDragged.id, event.target.id);
+      const taskContainer = document.querySelector('ul.taskContainer');
+      reRenderTask(tasksArray, taskContainer);
+      setTasksArray(tasksArray);
+    }
+  }, false);
+};
+
+export default dragAndDropHandler;
